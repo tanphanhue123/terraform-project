846870faadf61fb23bbe28c54a81a7fcf9e6f80f
tanphanhue123 <tanphanhue@gmail.com>
2023-03-13 19:17:47 +0700

    add all file

--- modules/database/outputs.tf
+++ modules/database/outputs.tf
@@ -1,5 +1,6 @@
-output "database_output" {
-    port = aws_db_instance.database.port
-    hostname = aws_db_instance.database.address
-    database = aws_db_instance.database.name
+output "database_config" {
+    value = {
+        hostname = aws_db_instance.database.address
+        database = aws_db_instance.database.name
+    }
 }
--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -2,8 +2,11 @@ output "vpc" {
     value = module.vpc
   
 }
+
 output "sg" {
-    lb = module.alb_sg.security_group.id
-    web = module.web_sg.security_group.id
-    db = module.db_sg.security_group.id
+    value = {
+        lb = module.alb_sg.security_group.id
+        web = module.web_sg.security_group.id
+        db = module.db_sg.security_group.id
+    }
 }
